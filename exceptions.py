3e83ac089de9b56d12ca1b2917d5f252a90d6534
Supreme Paudel <supremepaudel49@gmail.com>
2024-09-05 10:26:31 -0500

    Add exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+class InvalidArithmeticCommand(Exception):
+    pass
+
+class InvalidMemoryCommand(Exception):
+    pass
+
+class InvalidPointerIndex(Exception):
+    pass
+
+class InvalidCommand(Exception):
+    pass
+
