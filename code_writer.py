25e90d30d694d0732ab98c61242074a2e6bc1ab8
Supreme Paudel <supremepaudel49@gmail.com>
2024-09-06 14:43:39 -0500

    Modify code_writer to handle push for local,this,that,argument and constant

--- code_writer.py
+++ code_writer.py
@@ -1,9 +1,21 @@
 from __future__ import annotations
-from instruction import Arithmetic_instruction, Memory_instruction
+from instruction import Arithmetic_instruction, Memory_instruction, Instruction
 
 class Code_writer:
     
     __currentLabelNumber = 0
+    __memory_command_to_assembly_mapping = {
+            'local': "@LCL\n",
+            'argument': "@ARG\n",
+            'this':"@THIS\n",
+            'that':"@THAT\n",
+            'constant': "D=A",
+            'or': "M=M|D\n",
+            'eq': "D;JEQ\n",
+            'gt': "D;JGT\n",
+            'lt': "D;JLT\n",
+        }
+
 
     __arithmetic_command_to_assembly_mapping = {
             'add': "M=M+D\n",
@@ -23,8 +35,21 @@ class Code_writer:
         return "@SP\nM=M+1\n"
 
     @classmethod
+    def __push_d_register_onto_stack(cls):
+        # *SP = D
+        return "@SP\nA=M\nM=D\n"
+
+    @classmethod
     def __decrement_stack_pointer(cls):
         return "@SP\nM=M-1\n"
+
+    @classmethod
+    def __store_address_in_d_using_indirect_addressing(cls, ins: Instruction) -> str:
+        return f"@{cls.__memory_command_to_assembly_mapping[ins.memory_ins]}\nD=M\n@{ins.memory_index}\nA=A+D\nD=M"
+
+    @classmethod
+    def __load_constant(cls, value: str) -> str:
+        return f"@{value}\nD=A\n"
 
     @classmethod
     def __pop_value_into_d_register(cls):
@@ -35,13 +60,6 @@ class Code_writer:
         return "A=A-1\n"
 
     @classmethod
-    def __comment(cls, instruction):
-        if instruction.type == Arithmetic_instruction:
-            return f'// {instruction.arithmetic_ins}\n'
-        elif instruction.type == Memory_instruction:
-            return f'// {instruction.memory_ins} {instruction.memory_segment} {instruction.memory_segment}\n'
-    
-    @classmethod
     def __code_for_jump(cls, jump_instruction: str) -> str:
         num = cls.__currentLabelNumber
         cls.__currentLabelNumber += 1
@@ -49,7 +67,19 @@ class Code_writer:
 
     @classmethod
     def __move_d_in_sp_minus_two(cls) -> str:
-        return "@SP\nA=M-2\nM=D\n"
+        return "@SP\nA=M-1\nA=A-1\nM=D\n"
+
+    @classmethod
+    def __comment(cls, instruction):
+        if instruction.type == Arithmetic_instruction:
+            return f'// {instruction.arithmetic_ins}\n'
+        elif instruction.type == Memory_instruction:
+            return f'// {instruction.memory_ins} {instruction.memory_segment} {instruction.memory_segment}\n'
+
+    """ Public methods """ 
+    @classmethod
+    def set_file_name(cls, name: str) -> str:
+        cls.filename = name
 
     @classmethod
     def code(cls, instruction):
@@ -71,6 +101,13 @@ class Code_writer:
 
         # Handle memory instructions
         else:
-            pass
+            if instruction.memory_ins == 'push':
+                if instruction.memory_segment == 'constant':
+                    assembly_code = cls.__load_constant(instruction.memory_index)
+                else:
+                    assembly_code = cls.__store_address_in_d_using_indirect_addressing(instruction) 
+                assembly_code += cls.__push_d_register_onto_stack() + cls.__increment_stack_pointer()
+            else:
+                pass
 
 
